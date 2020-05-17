58140545ba0f4c140c309350825c73a9b86f9e16
Rikanthan <rikanthanricky@gmail.com>
2020-05-17 23:10:00 +0530

    animation 2

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -2,6 +2,7 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flash_chat/components/components.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static String id='welcome_screen';
@@ -69,46 +70,18 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
             SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, LoginScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, RegistrationScreen.id);
-                    //Go to registration screen.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
-              ),
-            ),
+            Rounded_button(title:'Login',colour: Colors.lightBlueAccent,onpress: () {
+    Navigator.pushNamed(context, LoginScreen.id);
+    //Go to registration screen.
+    }, ),
+            Rounded_button(title:'Register',colour: Colors.blueAccent,onpress: () {
+              Navigator.pushNamed(context, RegistrationScreen.id);
+              //Go to registration screen.
+            }, ),
           ],
         ),
       ),
     );
   }
 }
+
